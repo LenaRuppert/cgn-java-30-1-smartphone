16e4a8bf39f1878ebb91540742bc943e84cc46f8
lenaruppert <l.zi@gmx.de>
2023-01-30 18:19:09 +0100

    define methods

--- src/main/java/org/example/Smartphone.java
+++ src/main/java/org/example/Smartphone.java
@@ -9,18 +9,63 @@ public class Smartphone implements Radio, GPS {
 
     List<String> kontakts = new ArrayList<>();
 
+    Smartphone(){
+
+    }
+
+    public Smartphone(String model, String manufacturer, List<String> kontakts) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.kontakts = kontakts;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public List<String> getKontakts() {
+        return kontakts;
+    }
+
+    public void setKontakts(List<String> kontakts) {
+        this.kontakts = kontakts;
+    }
+
     @Override
     public String getPosition() {
-        return null;
+        return "Köln";
     }
 
     @Override
     public boolean startRadio() {
-        return false;
+        System.out.println("Radio startet");
+        return true;
     }
 
     @Override
     public boolean stopRadio() {
+        System.out.println("Radio stopped");
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "model='" + model + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", kontakts=" + kontakts +
+                '}';
     }
 }
